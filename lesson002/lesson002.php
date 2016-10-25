d5e31aa5bf5ed83fa36e903dd4c16bfd01c2cfee
Leo Koo <leokoo@gmail.com>
2016-10-26 03:56:13 +0800

    separating PHP Logic from presentation

---
+++ lesson002/lesson002.php
@@ -0,0 +1,8 @@
+<?php
+
+$mygreetings = 'Hello Malaysia!';
+
+//require calls the view file
+require 'lesson002.view.php';
+
+?>
---
+++ lesson002/lesson002.view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lessons in PHP 001</title>
+	<style>
+		header {
+			background: #e3e3e3;
+			padding: 2em;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<header>
+		<h1>
+			<?= $mygreetings ?>
+		</h1>
+	</header>
+</body>
+</html>
