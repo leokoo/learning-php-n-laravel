392cbd4408f4795537d03369ef62bc881bf61253
Leo Koo <leokoo@gmail.com>
2016-10-28 11:29:23 +0800

    booleans example with ternary operators

---
+++ lesson005-booleans/index.php
@@ -0,0 +1,12 @@
+<?php
+
+$task = [		'title' => 'Coding in PHP', 
+						'due' => 'tomorrow', 
+						'assigned_to' => 'Jimmy', 
+						'completed?' => false 
+					];
+
+//require calls the view file
+require 'index.view.php';
+
+?>
---
+++ lesson005-booleans/index.view.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Lessons in PHP 005</title>
+</head>
+<body>
+	<header>
+		<li>
+			<strong>Name:	</strong><?= $task['title']; ?>
+		</li>
+
+		<li>
+			<strong>Due Date:	</strong><?= $task['due']; ?>
+		</li>
+
+		<li>
+			<strong>Person Responsible:	</strong><?= $task['assigned_to']; ?>
+		</li>
+
+		<li>
+			<strong>Is it complete:	</strong><?= $task['completed?'] ? 'Completed!' : 'Not done yet'; ?>
+		</li>
+	</header>
+</body>
+</html>
