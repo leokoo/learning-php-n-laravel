f6e720487b2f3f89e95c2f1c03eb45c7c278a6da
Leo Koo <leokoo@gmail.com>
2016-11-03 17:41:05 +0800

    Classes 101 part 2

--- lesson008-classes/index.php
+++ lesson008-classes/index.php
@@ -1,5 +1,5 @@
 <?php
-//require 'functions.php';
+require 'functions.php';
 
 class Task {
 	// A function in a class is called a method
@@ -7,15 +7,25 @@ class Task {
 	// protected & public are encapsulation
 	protected $completed = false;
 
-	public function __construct( $description ) {
+	public function __construct( $description ) 
+	{
 	// __construct() is the constructor; automatically triggered on instantiation
 		$this->description = $description;
 		// We're setting a property to '$this' object
+	}
+
+	public function isComplete() 
+	{
+		return $this->completed;
 	}
 }
 
 $task  = new Task( 'Go to the store' );
 // Any arguments in the instantiation can be accepted in the constructor
+
+var_dump($task->isComplete());
+
+//dd($task);
 
 //require calls the view file
 //require 'index.view.php';
