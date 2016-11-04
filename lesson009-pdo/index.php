511bbbf01c2ff07921862ab471f17e7e315c2846
Leo Koo <leokoo@gmail.com>
2016-11-04 21:42:57 +0800

    learning exceptions

--- lesson009-pdo/index.php
+++ lesson009-pdo/index.php
@@ -1,6 +1,14 @@
 <?php
 // require 'functions.php';
 
+// Catching exceptions
+try {
+	$pdo = new PDO('mysql:host=127.0.0.1; dbname=mytodo', 'root', '');
+} catch (PDOException $e) {
+	die('Cannot connect lah');
+}
+
+
 //require calls the view file
 require 'index.view.php';
 ?>
