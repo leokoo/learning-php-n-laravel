13c9d6dfa1a51c717a64cc195ca84e4f69fce1de
vagrant <leokoo@gmail.com>
2016-11-08 11:49:28 +0000

    isolated more functions

--- lesson009-pdo/functions.php
+++ lesson009-pdo/functions.php
@@ -10,6 +10,17 @@ function connectToDb()
 	}
 }
 
+function fetchAllTasks($pdo)
+{
+	$statement = $pdo->prepare('select * from todos');
+
+	$statement->execute();
+
+//fetchAll fetches all results. We have to careful with this. Perhaps fetch is there's too many results
+//PDO::FETCH_OBJ stores each row into an object
+	return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
+
 function dd($input) {
 	echo '<pre>';
 		die(var_dump($input));
--- lesson009-pdo/index.php
+++ lesson009-pdo/index.php
@@ -2,16 +2,8 @@
 require 'functions.php';
 require 'Task.php';
 
-
 $pdo = connectToDb();
-
-$statement = $pdo->prepare('select * from todos');
-
-$statement->execute();
-
-//fetchAll fetches all results. We have to careful with this. Perhaps fetch is there's too many results
-//PDO::FETCH_OBJ stores each row into an object
-	$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+$tasks = fetchAllTasks($pdo);
 
 //require calls the view file
 require 'index.view.php';
