0d600c1aec0d8714612bba09b6b157e7116f513e
vagrant <leokoo@gmail.com>
2016-11-08 11:26:12 +0000

    cleaned up code

--- lesson009-pdo/functions.php
+++ lesson009-pdo/functions.php
@@ -1,5 +1,15 @@
 <?php
 
+function connectToDb()
+{
+	// Catching exceptions as we try to connect to our database
+	try {
+		return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'homestead', 'secret');
+	} catch (PDOException $e) {
+		die($e->getMessage()); //getMessage() will return a description of what went wrong
+	}
+}
+
 function dd($input) {
 	echo '<pre>';
 		die(var_dump($input));
--- lesson009-pdo/index.php
+++ lesson009-pdo/index.php
@@ -1,20 +1,17 @@
 <?php
-// require 'functions.php';
+require 'functions.php';
+require 'Task.php';
 
-// Catching exceptions as we try to connect to our database
-try {
-	$pdo = new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'homestead', 'secret');
-} catch (PDOException $e) {
-	die($e->getMessage()); //getMessage() will return a description of what went wrong
-}
 
-	$statement = $pdo->prepare('select * from todos');
+$pdo = connectToDb();
 
-	$statement->execute();
+$statement = $pdo->prepare('select * from todos');
+
+$statement->execute();
 
 //fetchAll fetches all results. We have to careful with this. Perhaps fetch is there's too many results
 //PDO::FETCH_OBJ stores each row into an object
-	$tasks = $statement->fetchAll(PDO::FETCH_OBJ);
+	$tasks = $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
 
 //require calls the view file
 require 'index.view.php';
