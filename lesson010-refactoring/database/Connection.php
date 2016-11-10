6e24675fc11eddabe9829948f148b85d48e77564
vagrant <leokoo@gmail.com>
2016-11-10 15:20:31 +0000

    PDO refactoring 1

---
+++ lesson010-refactoring/database/Connection.php
@@ -0,0 +1,19 @@
+<?php
+
+Class Connection
+{
+	public static function make() 
+	{
+		try {
+			return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'homestead', 'secret');
+		} catch (PDOException $e) {
+			die($e->getMessage()); //getMessage() will return a description of what went wrong
+		}
+	}
+
+}
+
+// To call a static function, we don't need a new instance. So..
+// Connection::make();
+// Thus to get a new connection, we do
+// $pdo = Connection::make();
--- lesson010-refactoring/functions.php
+++ lesson010-refactoring/functions.php
@@ -1,15 +1,5 @@
 <?php
 
-function connectToDb()
-{
-	// Catching exceptions as we try to connect to our database
-	try {
-		return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'homestead', 'secret');
-	} catch (PDOException $e) {
-		die($e->getMessage()); //getMessage() will return a description of what went wrong
-	}
-}
-
 function fetchAllTasks($pdo)
 {
 	$statement = $pdo->prepare('select * from todos');
