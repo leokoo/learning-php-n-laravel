434f2a8071ddceb1d1f5d1bb1627c4109fbc68b0
Leo Koo <leokoo@gmail.com>
2016-11-16 22:57:13 +0800

    moving passwords to config.php

--- lesson011-hidepasswords/database/Connection.php
+++ lesson011-hidepasswords/database/Connection.php
@@ -5,6 +5,8 @@ Class Connection
 	public static function make() 
 	{
 		try {
+			//We're trying to remove the hardcoded database info. Thus, we create config.php to call the data from the DB
+			//We don't want the password to be hardcoded, just in case there's an error and people gain access to it
 			return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'homestead', 'secret');
 		} catch (PDOException $e) {
 			die($e->getMessage()); //getMessage() will return a description of what went wrong
