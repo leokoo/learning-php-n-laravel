f5540c84a2967e96bb0a0c4a015b9edd4e9107a3
Leo Koo <leokoo@gmail.com>
2016-12-06 15:27:13 +0800

    separating routing to its own file

--- lesson012-makerouter/index.php
+++ lesson012-makerouter/index.php
@@ -2,11 +2,3 @@
 
 //We can switch from $query to anything we want, as long as it describe what it does. Like now, I've changed it to $db
 $db = require 'core/bootstrap.php';
-
-$routes = [
-	'' => 'controllers/index.php',
-	'about' => 'controllers/about.php',
-	'about-culture' => 'controllers/about-culture.php',
-	'contact' => 'controllers/contact.php'
-]
-?>
