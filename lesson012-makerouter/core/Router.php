5d03ca0f2dcc0bd1ec97eaee10b9a07fc3671874
Leo Koo <leokoo@gmail.com>
2016-12-06 22:26:54 +0800

    added a 'direct' method

--- lesson012-makerouter/core/Router.php
+++ lesson012-makerouter/core/Router.php
@@ -2,10 +2,19 @@
 
 class Router
 {
-	protected $routes;
+	protected $routes = [];
 
 	public function define($routes) 
 	{
 		$this->routes = $routes;
+	}
+
+	public function direct($uri)
+	{
+		if (array_key_exist($uri, $this->routes) {
+			return $this->routes['about/culture'];
+		}
+
+		throw new Exception('No routes defined for this URI');
 	}
 }
--- lesson012-makerouter/index.php
+++ lesson012-makerouter/index.php
@@ -2,3 +2,9 @@
 
 //We can switch from $query to anything we want, as long as it describe what it does. Like now, I've changed it to $db
 $db = require 'core/bootstrap.php';
+
+$router = new Router;
+
+require 'routes.php';
+
+require $router->direct();
