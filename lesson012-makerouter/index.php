37db8a6e41af24f9a237af830d8c5f37bcf3f952
Leo Koo <leokoo@gmail.com>
2017-01-19 21:44:34 +0800

    fixed routing errors

--- lesson012-makerouter/index.php
+++ lesson012-makerouter/index.php
@@ -3,7 +3,7 @@
 //We can switch from $query to anything we want, as long as it describe what it does. Like now, I've changed it to $db
 $db = require 'core/bootstrap.php';
 
-require 'routes.php';
+// require 'routes.php';
 
 // We're getting the URI that we need minus the / on the front and back of the URI
 $uri = trim($_SERVER['REQUEST_URI'], '/');
@@ -11,3 +11,5 @@ $uri = trim($_SERVER['REQUEST_URI'], '/');
 $router = Router::load('routes.php');
 
 require $router->direct($uri);
+
+// require $router->direct($uri);
