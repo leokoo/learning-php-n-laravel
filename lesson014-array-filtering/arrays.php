0e8b3aa3b4525ecb9b9d7384e88e0c6bd62357cd
Leo Koo <leokoo@gmail.com>
2017-01-27 17:27:27 +0800

    modified arrays to fetch the key 'title' instead

--- lesson014-array-filtering/arrays.php
+++ lesson014-array-filtering/arrays.php
@@ -18,8 +18,10 @@ $posts = [
 	new Post('My First Post', true),
 	new Post('My Second Post', true),
 	new Post('My Third Post', true),
-	new Post('My Last Post', false)
+	new Post('My Fourth Post', false)
 ];
+
+// var_dump($posts);
 
 // $unpublishedPosts = array_filter($posts, function ($post) {
 // 	// putting the bang ensures that only unpublished posts are shown in the var_dump results
@@ -29,9 +31,26 @@ $posts = [
 // var_dump($unpublishedPosts);
 
 // If we want to change all the statuses in the array to true, we then use array_map
-$modified = array_map(function ($post) {
-	$post->published = true;
-	return $post;
-}, $posts);
+// $modified = array_map(function ($post) {
+// 	$post->published = true;
+// 	return $post;
+// }, $posts);
 
-var_dump($modified);
+// var_dump($modified);
+
+// foreach ($posts as $index => $post) 
+// {
+// 	# code...
+// 	$posts[index] = (array) $post;
+// }
+$posts = array_map(function ($post) 
+	{
+		return(array) $post;
+	}, $posts);
+
+
+// var_dump($posts);
+// unlike the previous example, we're fetching a key called title.
+$titles = array_column($posts, 'title');
+
+var_dump($titles);
