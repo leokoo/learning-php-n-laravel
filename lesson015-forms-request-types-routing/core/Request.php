ddfedf8e4298bbcc9b76c02421763bc8e672745f
Leo Koo <leokoo@gmail.com>
2017-02-03 16:40:57 +0800

    fixed get request by retooling the uri method and using parse_url

--- lesson015-forms-request-types-routing/core/Request.php
+++ lesson015-forms-request-types-routing/core/Request.php
@@ -4,7 +4,10 @@ class Request
 {
 	public static function uri()
 	{
+		return trim(
+			parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
+			);
 		// We're getting the URI that we need minus the / on the front and back of the URI
-		return trim($_SERVER['REQUEST_URI'], '/');
+		// return trim($_SERVER['REQUEST_URI'], '/');
 	}
 }
