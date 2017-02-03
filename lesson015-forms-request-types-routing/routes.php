157a0413daaee18e7ad078a7c0a4c49c4a96c0af
Leo Koo <leokoo@gmail.com>
2017-02-03 16:18:43 +0800

    modified routes and added the 'add-name' controller

--- lesson015-forms-request-types-routing/routes.php
+++ lesson015-forms-request-types-routing/routes.php
@@ -4,5 +4,7 @@ $router->define([
 	'' => 'controllers/index.php',
 	'about' => 'controllers/about.php',
 	'about/culture' => 'controllers/about-culture.php',
-	'contact' => 'controllers/contact.php'
+	'contact' => 'controllers/contact.php',
+	// We add the 'names' route that when we have a request for that endpoint, we'll load up the add-name controller
+	'names' => 'controllers/add-name.php'
 ]);
