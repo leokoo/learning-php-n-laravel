4d74646fbf1c684be2c78aab94a30a6216962f29
Leo Koo <leokoo@gmail.com>
2017-02-03 19:10:08 +0800

    trying to fix routes error

--- lesson015-forms-request-types-routing/routes.php
+++ lesson015-forms-request-types-routing/routes.php
@@ -14,3 +14,5 @@ $router->get('', 'controllers/index.php');
 $router->get('about', 'controllers/about.php');
 $router->get('about/culture', 'controllers/about-culture.php');
 $router->post('names', 'controllers/add-name.php');
+
+var_dump($router->routes);
