d1c696d16743326fcefe3a7d4e6f391eddaaaabe
Leo Koo <leokoo@gmail.com>
2017-02-03 16:04:40 +0800

    fixed some form errors

--- lesson015-forms-request-types-routing/views/index.view.php
+++ lesson015-forms-request-types-routing/views/index.view.php
@@ -1,9 +1,10 @@
 <?php require('partials/head.php'); ?>
 
-// Creating a form for us to learn more about forms in PHP
+<!--Creating a form for us to learn more about forms in PHP-->
 <h1>Submit Your Name</h1>
 
 <form method="GET" action="/names">
-
+	<input name="name"></input>
 </form>
+
 <?php require('partials/footer.php'); ?>
