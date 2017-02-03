e00d293361c53273ccff8ce8dbc7876ee6e3e991
Leo Koo <leokoo@gmail.com>
2017-02-03 16:49:47 +0800

    modified var_dump's output

--- lesson015-forms-request-types-routing/core/Request.php
+++ lesson015-forms-request-types-routing/core/Request.php
@@ -5,6 +5,7 @@ class Request
 	public static function uri()
 	{
 		return trim(
+			// Parse_url ensures that the url is broken down the various components
 			parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/'
 			);
 		// We're getting the URI that we need minus the / on the front and back of the URI
