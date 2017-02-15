1437dbec72fc71cb212ba7bccb95aedb0d990486
Leo Koo <leokoo@gmail.com>
2017-02-15 18:00:16 +0800

    done with homework for lesson002

---
+++ lesson002-separating-php-logic-from-presentation/homework.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+	<style>
+		.greetings {
+			background-color: #00FFFF;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<div class="greetings">
+		<h2>
+			<?= 'How are you?' ?>
+		</h2>
+	<div>
+</body>
+</html>
--- lesson002-separating-php-logic-from-presentation/index.php
+++ lesson002-separating-php-logic-from-presentation/index.php
@@ -8,4 +8,6 @@ require 'lesson002.view.php';
 // Homework
 // Play around with creating a new file, preparing a few variables, and then loading a separate "view" file to present the HTML.
 
+require 'homework.view.php';
+
 ?>
