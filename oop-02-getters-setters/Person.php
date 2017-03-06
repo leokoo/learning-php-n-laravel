94094ba2694f8cb2c9b2ff72329d70cd96297899
Leo Koo <leokoo@gmail.com>
2017-03-06 17:00:16 +0800

    added the getter and setter methods and some comments

--- oop-02-getters-setters/Person.php
+++ oop-02-getters-setters/Person.php
@@ -8,9 +8,26 @@ class Person {
 	{
 		$this->name = $name;
 	}
+
+// Setter and Getter functions enables us to introduce behaviour however we need to
+	public function setAge($age)
+	{
+		if ($age < 18) {
+			throw new Exception('Person is not old enough');
+		}
+		$this->age = $age;
+	}
+
+	public function getAge()
+	{
+		return $this->age;
+	}
 }
 
 $john = new Person('John Doe');
-$john->age = 30;
+$john->setAge(30);
+/* The lack of encapsulation enables people to directly access the property
+e.g: $john->age = 3;
+That's why we need to proceed and encapsulate the age property */
 
-var_dump($john);
+var_dump($john->getAge());
