f5ea4ff8774a80cd625ef8c038ee3dd346659116
Leo Koo <leokoo@gmail.com>
2017-03-06 15:28:16 +0800

    class Task example

--- oop-bootcamp-01-classes/Task.php
+++ oop-bootcamp-01-classes/Task.php
@@ -1,5 +1,24 @@
 <?php
 
 class Task {
+	public $description;
+	public $completed = false;
 
+	public function __construct($description) 
+	{
+		$this->description = $description;
+	}
+
+	public function complete() 
+	{
+		$this->completed = true;
+	}
 }
+
+$task = new Task('Learn OOP in PHP');
+
+var_dump($task->description);
+
+$task->complete();
+
+var_dump($task->completed);
