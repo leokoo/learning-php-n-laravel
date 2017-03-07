03cedea058fec36fa2a5b9243a410f35f15c4378
Leo Koo <leokoo@gmail.com>
2017-03-07 22:01:12 +0800

    how to use a class as collection

--- oop-05-messages/ex.php
+++ oop-05-messages/ex.php
@@ -10,12 +10,25 @@ class Person {
 
 // The Business hires a Person. And the Person then joins the Staff
 class Business {
-	public function hire()
+	protected $staff;
+	// If the business needs a staff to function, we'll need to start with a staff
+	public function __construct(Staff $staff)
 	{
-		# code...
+		$this->staff = $staff;
+	}
+
+	public function hire(Person $person)
+	// by writing 'public function hire(Person $person)', we're using Type Hinting
+	{
+		$this->staff->add($person);
 	}
 }
 
 class Staff {
+	// Staff will serve as a collection
+	protected $members = [];
+	public function add(Person $person)
+	{
 
+	}
 }
