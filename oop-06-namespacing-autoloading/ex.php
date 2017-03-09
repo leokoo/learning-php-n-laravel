b5677f4a4992b38996dae22de31f5db4232acfe3
Leo Koo <leokoo@gmail.com>
2017-03-09 15:35:34 +0800

    cleared up some code

--- oop-06-namespacing-autoloading/ex.php
+++ oop-06-namespacing-autoloading/ex.php
@@ -1,5 +1,5 @@
 <?php
-require 'vendor/autoload.php';
+
 /*
 We can pull the files in using require, but it doesn't follow best practices
 e.g.:
@@ -8,13 +8,17 @@ require 'src/Business.php';
 require 'src/Staff.php';
 */
 
-$jeffrey = New Acme\Person('Jeffrey Way');
+Use Acme\Person;
+Use Acme\Business;
+Use Acme\Staff;
 
-$staff = New Acme\Staff([$jeffrey]);
+$jeffrey = New Person('Jeffrey Way');
+
+$staff = New Staff([$jeffrey]);
 // Because the business depends upon the $staff to run, we need to create a new instance of Staff before we can create a new instance of Business
-$laracasts = New Acme\Business($staff);
+$laracasts = New Business($staff);
 
 // $laracasts->hire($jeffrey);
-$laracasts->hire(New Acme\Person('Jane Doe'));
+$laracasts->hire(New Person('Jane Doe'));
 
 var_dump($laracasts->getStaffMembers());
