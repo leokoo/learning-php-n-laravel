83c28b8dec69325a4709ef111349812216a1eca9
Leo Koo <leokoo@gmail.com>
2017-03-09 18:40:21 +0800

    more examples on static methods

---
+++ oop-07-statics-constants/static.php
@@ -0,0 +1,33 @@
+<?php
+
+class Math {
+	public static function add(...$nums)
+	{
+		return array_sum($nums);
+	}
+}
+
+/* By adding a static to the add method, the way we call the method changes drastically
+	The add method now becomes like a global function
+	However, if the method begins to make calls to other classes, things can get messy quickly
+*/
+echo Math::add(1, 2, 3, 4);
+// $math = new Math;
+// var_dump($math->add(1, 2, 3, 4));
+
+class Person {
+	public static $age = 1;
+
+	public function haveBirthday()
+	{
+		static::$age += 1;
+	}
+}
+
+$joe = New Person;
+$joe->haveBirthday();
+$joe->haveBirthday();
+// If $joe has two birthdays, the result would be 3. However, if we have another person who has a birthday, $age will then be 4, not two. This is because static properties are shared
+$angela = New Person;
+$angela->haveBirthday();
+echo Person::$age;
