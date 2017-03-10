67c4cb0279ee0f4adf1b23c240238db94a7a1720
Leo Koo <leokoo@gmail.com>
2017-03-10 14:37:53 +0800

    created the listIngredients method to separate concerns

--- treehouse-oop/classes/render.php
+++ treehouse-oop/classes/render.php
@@ -1,17 +1,23 @@
 <?php
 
 class Render
-{	   
+{	
+	public static function listIngredients($ingredients)
+	{
+		$output = '';
+	  	foreach ($ingredients as $ingredient) {
+ 			$output .= $ingredient['amount'] . $ingredient['measure'] . " " . $ingredient['item']  . "\n";
+ 			}
+		return $output;
+	}
+
   public static function displayRecipe($recipe)
   {
   	/* $this->property can only be used in a method to reference a property, just like how we use the instantiated object name ($recipe1) outside of it */
   	$output = "";
   	$output .= $recipe->getTitle() . " by " . $recipe->getSource() . "\n";
   	$output .= implode(", ", $recipe->getTags()) . "\n\n";
-  	foreach ($recipe->getIngredients() as $ingredient) {
- 			$output .= $ingredient['amount'] . $ingredient['measure'] . " " . $ingredient['item']  . "\n";
- 			}
- 		$output .= "\n";
+ 		$output .= self::listIngredients($recipe->getIngredients());
 		$output .= implode("\n", $recipe->getInstructions()) . "\n";
 		$output .= $recipe->getYield() . "\n";
   	return $output;
