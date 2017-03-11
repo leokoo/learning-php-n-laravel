bfed42c172af37334a60243877a0598f5601cedf
Leo Koo <leokoo@gmail.com>
2017-03-11 23:47:52 +0800

    required the recipes database

--- treehouse-oop/cookbook.php
+++ treehouse-oop/cookbook.php
@@ -2,28 +2,7 @@
 // https://www.w3schools.com/php/php_includes.asp
 require 'classes/recipe.php';
 require 'classes/render.php';
-
-$recipe1 = new Recipe('My new recipe');
-
-// echo helps us display the property
-$recipe1->setSource('Fiona Ho');
-$malaysianRecipe = new Recipe();
-
-$recipe1->addIngredient('chicken', 2, 'LB');
-$recipe1->addIngredient('chinese mushrooms', 5, 'oZ');
-
-// On Instructions
-$recipe1->addInstruction('Get some eggs');
-$recipe1->addInstruction('fry the veggies');
-
-// On Tags
-$recipe1->addTag('Super Malaysian Breakfast');
-$recipe1->addTag('Delicious Nasi Lemak');
-
-// On Yield
-$recipe1->setYield('10 servings');
+require 'inc/allrecipes.php';
 
 // On Rendering
-// echo Render::displayRecipe($recipe1);
-echo $recipe1;
-echo new Render;
+echo Render::displayRecipe($belgian_waffles);
