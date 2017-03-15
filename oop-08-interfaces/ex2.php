f8a8a0aff3d9b290ee86e805ac6f756bcb1ff1b8
Leo Koo <leokoo@gmail.com>
2017-03-15 17:35:23 +0800

    more on interface

--- oop-08-interfaces/ex2.php
+++ oop-08-interfaces/ex2.php
@@ -1,27 +1,30 @@
 <?php
 // polymorphism & a strategy patterm
 interface Logger {
-	//
+	public function execute($message);
 }
 
-class LogToFile {
+class LogToFile implements Logger {
 	public function execute($message)
 	{
-			var_dump('log message to file :' . $message);
+			var_dump('log message to file : ' . $message);
 	}
 }
 
-class LogToDatabase {
+class LogToDatabase implements Logger {
 	public function execute($message)
 	{
-		var_dump('log message to database :' . $message);
+		var_dump('log message to database : ' . $message);
 	}
 }
 
 class UserController {
 	protected $logger;
-	// The problem with using LogToFile here is we're hardcoding a specific implementation, which causes problems later on. Here's where using Interface comes in
-	function __construct(LogToFile $logger)
+	/* The problem with using LogToFile here is we're hardcoding a specific implementation, which causes problems later on. Here's where using Interface comes in
+	Program to an interface, not an implementation
+	If there were ever any classes/tasks that we have multiple implementations and or methods of executing it, we need to create an interface
+	 */
+	function __construct(Logger $logger)
 	{
 		$this->logger = $logger;
 	}
@@ -34,5 +37,6 @@ class UserController {
 	}
 }
 
-$controller = new UserController(new LogToFile);
+// With interface, when we want to change from LogToFile to LogToDatabase, we don't have to change the UserController class
+$controller = new UserController(new LogToDatabase);
 $controller->show();
