82ec4709cd7d4dcfd91d9d069687804402798bb7
Leo Koo <leokoo@gmail.com>
2017-07-07 03:11:10 +0800

    testing out sprintf with inserting into sql

--- The PHP Practitioner/lesson016-Dynamic-Inserts-with-PDO/controllers/add-name.php
+++ The PHP Practitioner/lesson016-Dynamic-Inserts-with-PDO/controllers/add-name.php
@@ -1,11 +1,13 @@
 <?php
 
-var_dump('You typed the name ' . $_POST['name']); ?>
-<br>
+/* 
+	links :
+		http://php.net/manual/en/reserved.variables.post.php
 
-<?php echo "here's the database"; ?>
-<br>
-
-<?php
-var_dump($app['database']);
-//We are trying to save the information to the form and insert it into the database
+	because the id auto-increments, we only need to set the name
+	Thus, we'll take whatever the user types into the text box
+	Because $app[database] = new QueryBuilder, we'll need to make the new method in QueryBuilder.php
+*/
+$app['database']->insert('users-php1', [
+	'name' => $_POST['name'],
+]);
--- The PHP Practitioner/lesson016-Dynamic-Inserts-with-PDO/core/database/QueryBuilder.php
+++ The PHP Practitioner/lesson016-Dynamic-Inserts-with-PDO/core/database/QueryBuilder.php
@@ -1,16 +1,26 @@
 <?php
+
+/*
+	links
+		http://php.net/manual/en/pdo.prepare.php
+		http://php.net/manual/en/pdostatement.execute.php
+*/
 class QueryBuilder
 {
 	protected $pdo;
-	//for the query builder to do it's job, it depends on a pdo instance. That's why we use the constructor function.
-	//We accept the injected pdo from bootstrap.php and then we delegate it below.
+	/* 
+		for the query builder to do it's job, it depends on a pdo instance. That's why we use the constructor function.
+	 	We accept the injected pdo from bootstrap.php and then we delegate it below.
+	 */
 	public function __construct($pdo)
 	{
 		$this->pdo = $pdo;
 	}
 
-	//The query builder is responsible for building up queries.
-	//one method to select everything from a particular table
+	/*
+		The query builder is responsible for building up queries.
+		one method to select everything from a particular table
+	*/
 	public function selectAll($table)
 	{
 		$statement = $this->pdo->prepare("select * from {$table}");
@@ -18,7 +28,17 @@ class QueryBuilder
 		$statement->execute();
 	}
 
-	//How to build a new method to insert into the table?
+	// How to build a new method to insert into the table?
+	public function insert($table, $parameters) 
+	{
+		//  insert into %table_name (%column_name) 
+		// 	values (%column_values)
+		$sql = sprintf(
+			'insert into %s (%s) values (%s)',
+			'one', 'two', 'three'
+		);
+	die(var_dump($sql));
+	}
 
 
 }
